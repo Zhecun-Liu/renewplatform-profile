49a0e76922bf092781768de7872edcf3865cb58f
Kirk Webb <kwebb@cs.utah.edu>
2021-06-11 15:11:13 -0600

    Updates for operating in anechoic chamber, etc.

--- profile.py
+++ profile.py
@@ -40,18 +40,43 @@ import geni.rspec.emulab as elab
 import geni.rspec.emulab.spectrum as spectrum
 
 # Resource strings
-PCIMG = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
+PCIMG = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+MATLAB_DS_URN = "urn:publicid:IDN+emulab.net:powderprofiles+ltdataset+matlab-extra"
+MATLAB_MP = "/usr/local/MATLAB"
+STARTUP_SCRIPT = "/local/repository/faros_start.sh"
 PCHWTYPE = "d840"
 FAROSHWTYPE = "faros_sfp"
 IRISHWTYPE = "iris030"
+DEF_BS_SIZE = 0
+DEF_BS_MOUNT_POINT = "/opt/data"
+DEF_REMDS_MP = "/opt/data"
+
+REMDS_TYPES = [("readwrite", "Read-Write (persistent)"),
+               ("readonly", "Read Only"),
+               ("rwclone", "Read-Write Clone (not persistent)")]
+
+MMIMO_ARRAYS = ["", ("mmimo-ac", "Anechoic chamber array"),
+                ("mmimo1", "Honors rooftop array")]
+
+LAB_CLIENTS = ["", ("iris1-ac", "Anechoic chamber clients")]
 
 #
 # Profile parameters.
 #
 pc = portal.Context()
 
+# Array to allocate
+pc.defineParameter("mmimoid", "Name of  Massive MIMO array to allocate.",
+                   portal.ParameterType.STRING, MMIMO_ARRAYS[0], MMIMO_ARRAYS,
+                   longDescription="Leave blank to omit mMIMO array.")
+
+# Allocate client radio?
+pc.defineParameter("labclient", "Allocate lab Iris client radio.",
+                   portal.ParameterType.STRING, LAB_CLIENTS[0], LAB_CLIENTS,
+                   longDescription="Leave blank to omit lab client.")
+
 # Frequency/spectrum parameters
-portal.context.defineStructParameter(
+pc.defineStructParameter(
     "freq_ranges", "Range", [],
     multiValue=True,
     min=1,
@@ -73,6 +98,29 @@ portal.context.defineStructParameter(
         ),
     ])
 
+pc.defineParameter("fixedid", "Fixed PC Node_id (Optional)",
+                   portal.ParameterType.STRING, "", advanced=True,
+                   longDescription="Fix 'pc1' to this specific node.  Leave blank to allow for any available node of the correct type.")
+
+pc.defineParameter("matlabds", "Attach the Matlab dataset to the compute host.",
+                   portal.ParameterType.BOOLEAN, True, advanced=True)
+
+pc.defineParameter("hubints", "Number of interfaces to attach on hub (def: 2)",
+                   portal.ParameterType.INTEGER, 2, advanced=True,
+                   longDescription="This can be a number between 1 and 4.")
+
+pc.defineParameter("remds", "Remote Dataset (Optional)",
+                   portal.ParameterType.STRING, "", advanced=True,
+                   longDescription="Insert URN of a remote dataset to mount. Leave blank for no dataset.")
+
+pc.defineParameter("remmp", "Remote Dataset Mount Point (Optional)",
+                   portal.ParameterType.STRING, DEF_REMDS_MP, advanced=True,
+                   longDescription="Mount point for optional remote dataset.  Ignored if the 'Remote Dataset' parameter is not set.")
+
+pc.defineParameter("remtype", "Remote Dataset Mount Type (Optional)",
+                   portal.ParameterType.STRING, REMDS_TYPES[0], REMDS_TYPES,
+                   advanced=True, longDescription="Type of mount for remote dataset.  Ignored if the 'Remote Dataset' parameter is not set.")
+
 # Bind and verify parameters.
 params = pc.bindParameters()
 
@@ -81,61 +129,74 @@ for i, frange in enumerate(params.freq_ranges):
         perr = portal.ParameterError("Minimum and maximum frequencies must be separated by at least 1 MHz", ["freq_ranges[%d].freq_min" % i, "freq_ranges[%d].freq_max" % i])
         portal.context.reportError(perr)
 
+if params.hubints < 1 or params.hubints > 4:
+    perr = portal.ParameterError("Number of interfaces on hub to connect must be between 1 and 4 (inclusive).")
+    portal.context.reportError(perr)
+
 pc.verifyParameters()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
+# Mount a remote dataset
+def connect_DS(node, urn, mp, dsname = "", dstype = "rwclone"):
+    if not dsname:
+        dsname = "ds-%s" % node.name
+    bs = request.RemoteBlockstore(dsname, mp)
+    if dstype == "rwclone":
+        bs.rwclone = True
+    elif dstype == "readonly":
+        bs.readonly = True
+        
+    # Set dataset URN
+    bs.dataset = urn
+
+    # Create link from node to OAI dataset rw clone
+    bslink = request.Link("link_%s" % dsname, members=(node, bs.interface))
+    bslink.vlan_tagging = True
+    bslink.best_effort = True
+
 # Request a PC
 pc1 = request.RawPC("pc1")
-pc1.hardware_type = PCHWTYPE
-#pc1.component_id="pc19-meb"
+if params.fixedid:
+    pc1.component_id=params.fixedid
+else:
+    pc1.hardware_type = PCHWTYPE
 pc1.disk_image = PCIMG
-bs = request.RemoteBlockstore( "matlab1", "/usr/local/MATLAB" )
-bs.dataset = "urn:publicid:IDN+emulab.net:powderprofiles+ltdataset+matlab-extra"
-bs.rwclone = True
-bslink = request.Link( "dslink1" )
-bslink.addInterface( pc1.addInterface( "dsiface1" ) )
-bslink.addInterface( bs.interface )
-bslink.vlan_tagging = True
-bslink.best_effort = True
-pc1.addService(pg.Execute(shell="sh", command="/usr/bin/sudo /local/repository/faros_start.sh"))
+if params.matlabds:
+    connect_DS(pc1, MATLAB_DS_URN, MATLAB_MP, "matlab1")
+pc1.addService(pg.Execute(shell="sh", command=STARTUP_SCRIPT))
 if1pc1 = pc1.addInterface("if1pc1", pg.IPv4Address("192.168.1.1", "255.255.255.0"))
-if1pc1.bandwidth = 40 * 1000 * 1000 # 40 Gbps
+#if1pc1.bandwidth = 40 * 1000 * 1000 # 40 Gbps
 bs2 = pc1.Blockstore("scratch","/scratch")
 bs2.placement = "nonsysvol"
+if params.remds:
+    connect_DS(pc1, params.remds, params.remmp, dstype=params.remtype)
+
+# LAN connecting up everything (if needed).  Members are added below.
+lan1 = None
+if params.mmimoid or params.labclient:
+    lan1 = request.LAN("lan1")
+    lan1.vlan_tagging = False
+    lan1.setNoBandwidthShaping()
+    lan1.addInterface(if1pc1)
 
 # Request a Faros BS.
-mm1 = request.RawPC("mm1")
-mm1.hardware_type = FAROSHWTYPE
-mm1if1 = mm1.addInterface("if1")
-mm1if2 = mm1.addInterface("if2")
-mm1if3 = mm1.addInterface("if3")
-#mm1if4 = mm1.addInterface("if4")
+if params.mmimoid:
+    mm1 = request.RawPC("mm1")
+    mm1.component_id = params.mmimoid
+    mm1.hardware_type = FAROSHWTYPE
+    for i in range(params.hubints):
+        mmif = mm1.addInterface()
+        lan1.addInterface(mmif)
 
-# Request an Iris client.
-ir1 = request.RawPC("ir1")
-ir1.hardware_type = IRISHWTYPE
-ir1.component_id = "iris03"  # in Mike's office
-ir1if1 = ir1.addInterface("if1")
-
-# Request another Iris client.
-ir2 = request.RawPC("ir2")
-ir2.hardware_type = IRISHWTYPE
-ir2.component_id = "iris04"  # in Jon's office
-ir2if1 = ir2.addInterface("if1")
-
-# Connect the PC, BS, and Iris clients to a LAN
-lan1 = request.LAN("lan1")
-lan1.vlan_tagging = False
-lan1.setNoBandwidthShaping()
-lan1.addInterface(if1pc1)
-lan1.addInterface(mm1if1)
-lan1.addInterface(mm1if2)
-lan1.addInterface(mm1if3)
-#lan1.addInterface(mm1if4)
-lan1.addInterface(ir1if1)
-lan1.addInterface(ir2if1)
+# Lab client to allocate (if any).
+if params.labclient:
+    labir = request.RawPC("labir1")
+    labir.hardware_type = IRISHWTYPE
+    labir.component_id = params.labclient
+    labif = labir.addInterface()
+    lan1.addInterface(labif)
 
 # Add frequency request(s)
 for frange in params.freq_ranges:
