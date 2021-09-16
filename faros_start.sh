b19c179cdde280c2513cb1b1f1533eec30264446
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2021-09-16 10:13:52 -0600

    Update faros_start.sh

--- faros_start.sh
+++ faros_start.sh
@@ -5,7 +5,6 @@ MYWD=`dirname $0`
 SCRATCH="/scratch"
 REPO="https://github.com/renew-wireless/RENEWLab.git"
 PYFAROS="https://github.com/skylarkwireless/pyfaros.git"
-SOUNDER="/scratch/RENEWLab/CC/Sounder/"
 
 if [ -z $IF1 ]
 then
@@ -41,34 +40,75 @@ fi
 sudo service isc-dhcp-server start || \
     { echo "Failed to start ISC dhcpd!" && exit 1; }
 
+
 cd $SCRATCH
 sudo chown ${USER}:${GROUP} .
 sudo chmod 775 .
-git clone $REPO || \
+
+mkdir dependencies
+mkdir dev_repos
+cd dev_repos
+
+git clone --branch master $REPO || \
     { echo "Failed to clone git repository: $REPO" && exit 1; }
 
-cd RENEWLab
-sudo ./install_soapy.sh || \
-    { echo "Failed to install Soapy!" && exit 1; }
+cd ../dependencies
 
-sudo ./install_pylibs.sh || \
-    { echo "Failed to install Python libraries!" && exit 1; }
+# --- Armadillo (10.6.2)
+wget http://sourceforge.net/projects/arma/files/armadillo-10.6.2.tar.xz
+tar -xf armadillo-10.6.2.tar.xz
+cd armadillo-10.6.2
+cmake -DALLOW_OPENBLAS_MACOS=ON .
+make -j`nproc`
+sudo make install
+sudo ldconfig
 
-sudo ./install_cclibs.sh || \
-    { echo "Failed to install C libraries!" && exit 1; }
+# Install Soapy tools
+# SoapySDR
+git clone --branch soapy-sdr-0.8.1 --depth 1 --single-branch https://github.com/pothosware/SoapySDR.git
+cd SoapySDR
+mkdir -p build
+cd build
+cmake ../
+make -j`nproc`
+sudo make install
+cd ../..
+sudo ldconfig
 
-sudo apt-get -q -y install python3-pip
-sudo pip3 install --upgrade pip
+#SoapyRemote
+git clone --branch soapy-remote-0.5.2 --depth 1 --single-branch https://github.com/pothosware/SoapyRemote.git
+cd SoapyRemote
+mkdir -p build
+cd build
+cmake ../
+make -j`nproc`
+sudo make install
+cd ../..
+sudo ldconfig
+
+#Iris drivers
+git clone --branch soapy-iris-2020.02.0.1 --depth 1 --single-branch https://github.com/skylarkwireless/sklk-soapyiris.git
+cd sklk-soapyiris
+mkdir -p build
+cd build
+cmake ../
+make -j`nproc`
+sudo make install
+cd ../..
+sudo ldconfig
 
 git clone --branch v1.1 --depth 1 --single-branch $PYFAROS || \
     { echo "Failed to clone git repository: $PYFAROS" && exit 1; }
 cd pyfaros/
 ./create_package.sh
-cd dist && pip3 install *.tar.gz --ignore-installed || \
+cd dist && sudo pip3 install pyfaros-0.0.5+efa49b90.tar.gz --ignore-installed || \
     { echo "Failed to install Pyfaros!" && exit 1; }
 
+#export PYTHONPATH=/usr/local/lib/python3/dist-packages/:"${PYTHONPATH}"
+#echo /usr/local/lib/python3/dist-packages/ | sudo tee /usr/lib/python3/dist-packages/SoapySDR.pth
+sudo ldconfig
+
 # Run command twice in case of board discovery transient issue
-cd $SOUNDER"files"
 python3 -m pyfaros.discover --json-out
 sleep 1
 python3 -m pyfaros.discover --json-out
