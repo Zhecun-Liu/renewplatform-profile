f96a2053a9420e95a321e66b4a99f546fd58218d
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2023-07-25 17:04:03 -0600

    Update renew_start.sh

--- renew_start.sh
+++ renew_start.sh
@@ -112,9 +112,9 @@ git clone --branch develop $RENEWLAB RENEWLab || \
 cd ../dependencies
 
 # --- Armadillo (12.2.0)
-wget http://sourceforge.net/projects/arma/files/armadillo-12.2.0.tar.xz
-tar -xf armadillo-12.2.0.tar.xz
-cd armadillo-12.2.0
+wget http://sourceforge.net/projects/arma/files/armadillo-12.6.0.tar.xz
+tar -xf armadillo-12.6.0.tar.xz
+cd armadillo-12.6.0
 cmake -DALLOW_OPENBLAS_MACOS=ON .
 make -j`nproc`
 sudo make install
