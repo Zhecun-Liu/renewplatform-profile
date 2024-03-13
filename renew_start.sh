1028d413ad64a2cb3481df719a3b42488bf2fc47
zhecunliu <zl83@rice.edu>
2024-03-13 15:43:44 -0500

    fix boost lib bug

--- renew_start.sh
+++ renew_start.sh
@@ -136,7 +136,7 @@ UHD=true
 if [ "$UHD" = true ] ; then
 	echo Installing UHD driver...
   # updated dependencies setup
-	# sudo apt install -y libboost-all-dev libusb-1.0-0-dev doxygen
+	sudo apt install -y libboost-all-dev libusb-1.0-0-dev doxygen
   sudo apt-get install autoconf automake build-essential ccache cmake cpufrequtils doxygen ethtool \
   g++ git inetutils-tools libboost-all-dev libncurses5 libncurses5-dev libusb-1.0-0 libusb-1.0-0-dev \
   libusb-dev python3-dev python3-mako python3-numpy python3-requests python3-scipy python3-setuptools \
